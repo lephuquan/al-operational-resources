17ec16aee174816b524896cd823b2b54c927321c
Le Phu Quan <lephuquan.dev@gmail.com>
2026-04-06 23:23:24 +0700

    Initial commit with project setup and basic structure established.

---
+++ src/main/java/com/programming_with_al/al_operational_resources/AlOperationalResourcesApplication.java
@@ -0,0 +1,13 @@
+package com.programming_with_al.al_operational_resources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlOperationalResourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlOperationalResourcesApplication.class, args);
+	}
+
+}
